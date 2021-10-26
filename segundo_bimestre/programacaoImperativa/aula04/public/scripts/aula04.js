ad49980e7e30bda4760db7f46a34d280bba672d3
DevLuizSouza <inboxluizsouza@gmail.com>
2021-10-26 19:15:30 -0300

    Commit das aulas 4

---
+++ segundo_bimestre/programacaoImperativa/aula04/public/scripts/aula04.js
@@ -0,0 +1,22 @@
+
+console.log("Hello World");
+var x = 8;
+var y = 5; 
+const z = 25;
+let h = 96;
+h = 78;
+x = 5; 
+console.log(x, y, z, h);
+
+let nome = 'Luiz';
+console.log(nome); 
+
+let plano = true; 
+console.log(plano);
+
+let soma = h + x;
+let sub = h - x; 
+let mult = h * x; 
+let div = h / x; 
+
+console.log(soma, sub, mult, div); 
---
+++ segundo_bimestre/programacaoImperativa/aula04/public/scripts/imc.js
@@ -0,0 +1,9 @@
+const nome = "Ana Paula"; 
+let idade = 26;
+let peso = 55.0;
+let altura = 1.62; 
+let plano = true; 
+
+let imc = Math.round(peso / (altura * altura)); 
+console.log(nome, 'tem ', idade, ' anos e seu índice de massa corporal é de ', imc); 
+
